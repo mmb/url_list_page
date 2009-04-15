65566c94edbe20a013fefabb0d8cdbcd6368d51a
Matthew M. Boedicker <matthewm@boedicker.org>
2009-04-14 18:53:11 -0700

    add command line options, rdoc usage, more input/output options

--- url_list_page.rb
+++ url_list_page.rb
@@ -1,21 +1,54 @@
+# == Usage
+#   url_list_page.rb input output [options]
+#
+#   input is a file path or url or - for stdin
+#   output is a file path
+#
+# == Options
+#   -h, --help             displays help message
+#   -c, --css CSS_URL      css url
+#   -t, --title PAGE_TITLE page title
+
 require 'embed_flickr'
 require 'embed_img'
 require 'embed_vimeo'
 require 'embed_youtube'
 require 'urltitles'
 
-require 'yaml'
-
 require 'rubygems'
 require 'builder'
 
-Options = {
+require 'open-uri'
+require 'optparse'
+require 'rdoc/usage'
+require 'yaml'
+
+options = { 
   :css => 'http://matthewm.boedicker.org/screen.css',
   :title => 'urls',
   :meta => [
     {:name => 'viewport', :content => 'initial-scale=1.0'},
     ],
-  }
+}
+
+if ARGV.length < 2
+  RDoc::usage()
+else
+  if ARGV[0] == '-'
+    options[:in] = STDIN
+  else
+    options[:in] = open(ARGV[0])
+  end
+
+  options[:out] = ARGV[1]
+end
+
+OptionParser.new do |opts|
+  opts.on('-c', '--css CSS_URL', 'css url') { |css| options[:css] = css }
+  opts.on('-t', '--title PAGE_TITLE', 'page title') do |title|
+    options[:title] = title
+  end
+end.parse!(ARGV[2..-1])
 
 $url_titles = UrlTitles::UrlTitles.new('url_titles_cache.yaml')
 
@@ -47,22 +80,22 @@ xm = Builder::XmlMarkup.new
 xm.instruct! :xml
 xm.declare! :DOCTYPE, :html, :PUBLIC, '-//W3C//DTD XHTML 1.1//EN',
   'http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd'
-puts xm.html(:xmlns => 'http://www.w3.org/1999/xhtml',
+output = xm.html(:xmlns => 'http://www.w3.org/1999/xhtml',
   :'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
   :'xsi:schemaLocation' => 'http://www.w3.org/MarkUp/SCHEMA/xhtml11.xsd',
   :'xml:lang' => 'en') {
   xm.head {
-    xm.title(Options[:title]) if Options[:title]
-    Options.fetch(:meta, []).each { |m| xm.meta(m) }
-    if Options[:css]
-      xm.link(:rel => 'stylesheet', :type => 'text/css', :href => Options[:css])
+    xm.title(options[:title]) if options[:title]
+    options.fetch(:meta, []).each { |m| xm.meta(m) }
+    if options[:css]
+      xm.link(:rel => 'stylesheet', :type => 'text/css', :href => options[:css])
     end
     xm.script('', :type => 'text/javascript',
       :src => 'http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js')
   }
   xm.body {
     xm.ul {
-      STDIN.each_line do |url|
+      options[:in].each_line do |url|
         url.strip!
         xm.li(:class => (STDIN.lineno % 2 == 0 ? 'even' : 'odd')) {
           xm.a($url_titles.get(url).strip, :href => url)
@@ -87,6 +120,8 @@ puts xm.html(:xmlns => 'http://www.w3.org/1999/xhtml',
     }
   }
 }
+
+open(options[:out], 'w') { |f| f.write(output) }
 
 EmbedFlickr::THUMBS_CACHE.save
 EmbedVimeo::HTMLS_CACHE.save
